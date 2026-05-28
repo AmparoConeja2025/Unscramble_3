1e34da7cf09284c02d7259d332936d131f5daa8b
AmparoConeja2025 <randallb59@yahoo.com>
2026-05-28 07:58:45 -0500

    Initial commit from Unscramble_2

---
+++ sw.js
@@ -0,0 +1,4 @@
+self.addEventListener('fetch', (event) => {
+  // This event listener is required for a true PWA install prompt on Chrome.
+  // It doesn't need to do anything for now; its presence is enough.
+});
