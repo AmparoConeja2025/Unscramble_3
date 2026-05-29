53500233d469a9d35b7360ebc2c6beb1be4c073c
AmparoConeja2025 <randallb59@yahoo.com>
2026-05-29 04:12:23 -0500

    Strip lessons data into lessons.js v7

--- lessons.js
+++ lessons.js
@@ -8,5 +8,15 @@ const lessons = [
         correctOrder: ["I", "told", "her", "not", "to", "come", "over"],
         english: "I told her not to come over.",
         spanish: "Mi mejor amiga quería visitarme en mi casa, pero le dije que no."
+    },
+    {
+        id: 2,
+        category: "people",
+        narratorAudio: "Audio/people-dianne-best-friend-story.mp3",
+        answerAudio: "Audio/people-dianne-best-friend-answer.mp3",
+        sentence: ["I", "told", "her", "not", "to", "come", "over"],
+        correctOrder: ["I", "told", "her", "not", "to", "come", "over"],
+        english: "I told her not to come over.",
+        spanish: "Mi mejor amiga quería visitarme en mi casa, pero le dije que no."
     }
 ];
