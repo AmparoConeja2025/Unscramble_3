1509f5829147a0c13db0f420f75307bad68f71cc
AmparoConeja2025 <randallb59@yahoo.com>
2026-05-29 17:14:52 -0500

    update

--- lessons.js
+++ lessons.js
@@ -20,5 +20,28 @@ const lessons = [
         correctOrder: ["I", "ran", "into", "my", "friend", "while", "I", "was", "shopping"],
         english: "I ran into my friend while I was shopping.",
         spanish: "Me encontré con mi amigo el otro día en el mercado."
-    }
-];
+    },
+    {
+        id: 3,
+        narrator: "laura",
+        category: "work",
+        narratorAudio: "Audio/work-laura-go-without-story.mp3",
+        answerAudio: "Audio/work-laura-go-without-answer.mp3",
+        sentence: ["I", "guess", "I'll", "have", "to", "go", "without"],
+        correctOrder: ["I", "guess", "I'll", "have", "to", "go", "without"],
+        english: "I guess I'll have to go without.",
+        spanish: "No tengo dinero para comprar el almuerzo."
+    },
+    {
+    id: 4,
+    narrator: "john",
+    category: "people",
+    narratorAudio: "Audio/people-john-look-after-story.mp3",
+    answerAudio: "Audio/people-john-look-after-answer.mp3",
+    sentence: ["My", "father", "is", "going", "to", "look", "after", "her"],
+    correctOrder: ["My", "father", "is", "going", "to", "look", "after", "her"],
+    english: "My father is going to look after her.",
+    spanish: "Mi mamá está en la cama con la gripa. Mi papá la va a cuidar."
+},
+    
+    ];
